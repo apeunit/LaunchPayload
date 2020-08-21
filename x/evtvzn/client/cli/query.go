ab93f2452031249314cff00bcbca031df97a719d
Developer Experience team at Tendermint <hello@tendermint.com>
2020-08-21 13:39:36 +0200

    Initialized with Starport

---
+++ x/evtvzn/client/cli/query.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"fmt"
+	// "strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+
+	// "github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	// sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/apeunit/evtvzn/x/evtvzn/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	// Group evtvzn queries under a subcommand
+	evtvznQueryCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	evtvznQueryCmd.AddCommand(
+		flags.GetCommands(
+      // this line is used by starport scaffolding
+		)...,
+	)
+
+	return evtvznQueryCmd
+}
